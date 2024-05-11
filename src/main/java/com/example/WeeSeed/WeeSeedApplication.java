5c359b0100c66665e179fd7b174151393a11bbf3
changeme4585 <93308824+changeme4585@users.noreply.github.com>
2024-05-11 21:25:37 +0900

    first commit

---
+++ src/main/java/com/example/WeeSeed/WeeSeedApplication.java
@@ -0,0 +1,13 @@
+package com.example.WeeSeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeeSeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeeSeedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/WeeSeed/WeeSeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WeeSeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeeSeedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
