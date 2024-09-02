9de5f3068b34cd6da7ae6ba317028acf62cc73f9
changeme4585 <benscience@naver.com>
2024-09-02 19:25:15 +0900

    feat: 비밀번호 변경 로직 추가

--- src/main/java/com/example/WeeSeed/service/FindUserService.java
+++ src/main/java/com/example/WeeSeed/service/FindUserService.java
@@ -1,16 +1,24 @@
 package com.example.WeeSeed.service;
 
 
+import com.example.WeeSeed.entity.User;
 import com.example.WeeSeed.repository.FindUserInfoRepository;
+import com.example.WeeSeed.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class FindUserService {
     private  final FindUserInfoRepository findUserInfoRepository;
-
+    private  final UserRepository userRepository;
     public String findUserId(String name,String email){
         return findUserInfoRepository.findUserId(name,email);
+    }
+    public void  changeUserPass(String userId,String password){
+       User  user = userRepository.findByUserId(userId).get();
+       user.updateUserPass(password);
     }
 }
