ebea265de2cf86ba3bdb4385c2d819ca6e19d28d
changeme4585 <benscience@naver.com>
2024-09-11 18:39:39 +0900

    feat: 변경된 비밀번호를 암호화해서 db에 저장하는 로직

--- src/main/java/com/example/WeeSeed/service/FindUserService.java
+++ src/main/java/com/example/WeeSeed/service/FindUserService.java
@@ -1,16 +1,19 @@
 package com.example.WeeSeed.service;
 
 
+import com.example.WeeSeed.Encrypt;
 import com.example.WeeSeed.entity.User;
 import com.example.WeeSeed.repository.FindUserInfoRepository;
 import com.example.WeeSeed.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class FindUserService {
     private  final FindUserInfoRepository findUserInfoRepository;
     private  final UserRepository userRepository;
@@ -19,6 +22,8 @@ public class FindUserService {
     }
     public void  changeUserPass(String userId,String password){
        User  user = userRepository.findByUserId(userId).get();
-       user.updateUserPass(password);
+        Encrypt en = new Encrypt(password);
+        String encryptedPassword = en.getEncryptedPassword();
+       user.updateUserPass(encryptedPassword);
     }
 }
