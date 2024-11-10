962d93d2629399350079517300026967860083b2
changeme4585 <benscience@naver.com>
2024-11-10 16:57:02 +0900

    feat: gpt Controller 코드

---
+++ src/main/java/com/example/WeeSeed/controller/ChatGptController.java
@@ -0,0 +1,19 @@
+package com.example.WeeSeed.controller;
+
+import com.example.WeeSeed.service.ChatGptService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class ChatGptController {
+    private  final ChatGptService chatGptService;
+
+
+    @GetMapping (value = "/gpt_test")
+    public  String gptTest(@RequestParam("gpt") String gpt){
+        return chatGptService.question(gpt);
+    }
+}
