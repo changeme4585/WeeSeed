ba8df8946913559e8811e3d736b83fe60285f100
changeme4585 <benscience@naver.com>
2024-11-11 17:11:04 +0900

    feat: 챗봇 기능 추가

--- src/main/java/com/example/WeeSeed/controller/ChatGptController.java
+++ src/main/java/com/example/WeeSeed/controller/ChatGptController.java
@@ -1,6 +1,7 @@
 package com.example.WeeSeed.controller;
 
 import com.example.WeeSeed.service.ChatGptService;
+import com.example.WeeSeed.service.ChatService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,12 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 public class ChatGptController {
-    private  final ChatGptService chatGptService;
+    private  final ChatService chatService;
 
 
     @GetMapping (value = "/gpt_test")
     public  String gptTest(@RequestParam("gpt") String gpt){
-        //
-        return chatGptService.question(gpt);
+
+        return chatService.question(gpt);
     }
 }
