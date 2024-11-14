b2b80a1a7c3692d03d94a0ffa50b32f0aabf518b
heily-tech <edelbteen@gmail.com>
2024-11-14 16:16:14 +0900

    feat : integrated codes excluding AI

--- src/main/java/com/example/WeeSeed/repository/ChildRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.WeeSeed.repository;
-
-import com.example.WeeSeed.entity.Child;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChildRepo extends JpaRepository<Child, String> {
-}
-
